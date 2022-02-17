d9908a926411657f06c743abd534abd68d2b3209
linjianing <linjianing@linklogis.com>
2022-02-17 18:19:23 +0800

    test combiner

---
+++ test/test_transform/test_combiner.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 17 14:47:48 2022
+
+@author: linjianing
+"""
+
+import pandas as pd
+
+from chiascal.utils.comms import make_x_y
+from chiascal.transform.combiner import Combiner
+from chiascal.selection import StatsSelector
+
+
+test_sample = pd.read_hdf(r'.\test\data\test_sample.h5', 'data')
+sample_X, sample_y = make_x_y(test_sample, 'flag')
+statsselector = StatsSelector(n_jobs=1, noconcentration=0.5)
+sample_X = statsselector.fit_transform(sample_X, sample_y)
+comb = Combiner()
+comb.fit(sample_X, sample_y)
