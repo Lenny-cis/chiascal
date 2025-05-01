472d69678a295906bf3bd4cc5b87f94ed57e7316
Lenny-cis <ljn9008@163.com>
2025-05-01 14:37:06 +0800

    Update __init__.py

--- chiascal/selection/__init__.py
+++ chiascal/selection/__init__.py
@@ -5,5 +5,5 @@ Created on Wed Feb 16 17:43:24 2022
 @author: linjianing
 """
 
-from .statsselector import StatsSelector
-from .modelselector import TreeSelector, LassoLRCV, StepwiseSelector
+from .statsselector import StatsSelector, PSISelector
+from .modelselector import TreeSelector, LassoLRCV, StepwiseSelector, HyperoptLgb
