b7c6d36140fe1fcb26111d28ace28ab43479035f
Lenny-cis <ljn9008@163.com>
2025-05-01 17:39:54 +0800

    Update corrgraph.py

--- chiascal/collinear/corrgraph.py
+++ chiascal/collinear/corrgraph.py
@@ -20,19 +20,26 @@ logger = logging.getLogger(__name__)
 class CorrGraphSelector(TransformerMixin, BaseEstimator):
     """连通图筛选相关变量."""
 
-    def __init__(self, corr_threshold=0.8):
-        self.corr_threshold = corr_threshold
+    def __init__(self, corr=0.8, method='spearman):
+        self.corr = corr
+    	self.method = method
 
+	@FuncRunInfo(logger)
     def fit(self, X, y=None, iv_func=None):
         """训练."""
         logger.info('Start {} fit'.format(self.__class__.__name__))
         num_vars = [name for name, dtp in X.dtypes.items()
                     if pd.api.types.is_numeric_dtype(dtp)]
-        corr = X.loc[:, num_vars].corr()
-        corr_g = nx.from_pandas_adjacency(corr)
+        if self.method == 'spearman':
+            corr = X.loc[:, num_vars].rank().corr()
+        else:
+	        corr = X.loc[:, num_vars].corr(method=self.method)
+        corr_g = nx.from_pandas_adjacency(corr.fillna(0))
         corr_g.remove_edges_from(nx.selfloop_edges(corr_g))
         _ = [corr_g.remove_edge(*e) for e, w in corr_g.edges().items()
-             if w['weight'] < self.corr_threshold]
+             if w['weight'] < self.corr]
+        if iv_func is None:
+            iv_func = X.std
         node_iv = {key: {'ivval': val} for key, val in iv_func().items()}
         nx.set_node_attributes(corr_g, node_iv)
         conn_comps = [c for c in nx.connected_components(corr_g)]
@@ -40,8 +47,22 @@ class CorrGraphSelector(TransformerMixin, BaseEstimator):
             corr_g.subgraph(comp), 'ivval').items(), key=lambda x: x[1],
             reverse=True)[0][0] for comp in conn_comps]
         res_var.extend(X.columns.difference(num_vars))
-        self.corr_vars = res_var
+        self.input_vars = X.columns.tolist()
+        self.output_vars = res_var
+        self.corr_mat = corr
 
     def transform(self, X):
         """应用."""
-        return X.loc[:, self.corr_vars]
+        trn_x_name = [x for x in X.columns if x in self.output_vars]
+        return X.loc[:, trn_x_name]
+
+	@property
+    def rept(self):
+        """Report DF."""
+        in_num = len(self.input_vars)
+        ou_num = len(self.output_vars)
+        summ = pd.DataFrame.from_dict({
+            'CORR': {'input': in_num, 'filter': in_num-ou_num,
+                     'output': ou_num}},
+            orient='index')
+        return Report_tuple(summ, self.corr_mat)
